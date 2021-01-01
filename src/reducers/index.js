680bc46f34ada5a8f6f424fe405184ce5e8adf79
Ly Gia Dieu <lygiadieu@Lys-MacBook-Pro.local>
2021-01-01 16:35:49 +0700

    commit - SettingUpRedux

---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+import {combineReducers}  from 'redux';
+import successReducer from './SuccessReducers';
+
+export default combineReducers({
+  successReducer,
+});
---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import RootReducer from '../src/reducers';
+
+
+const middlewares = [thunk];
+
+const store = createStore(RootReducer, composeWithDevTools(
+    applyMiddleware(...middlewares),
+    // ...middlewares này có prototype là array => sẽ sử dụng đầy đủ tính năng của array. Gọn hơn rất nhiều phải không ? Với tham số ...args của display method chúng ta nhận bao nhiêu đối số cũng được, hay nói cách khác là chúng ta có thể "spread" các tham số truyền vào.
+));
+
+export default store;
