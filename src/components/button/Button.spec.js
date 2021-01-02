3ca516cb170dc47b0c248f362efe8ed61916e141
GiaDieu <giadieuly@gmail.com>
2021-01-02 20:09:23 +0700

    commit -m SimulateEvent

--- src/components/button/Button.spec.js
+++ src/components/button/Button.spec.js
@@ -1,35 +1,44 @@
 import React from 'react';
-import {shallow} from 'enzyme';
-import { findByTestAttr , checkProps} from '../../../utils';
+import { shallow } from 'enzyme';
+import { findByTestAttr, checkProps } from '../../../utils';
 import SharedButton from './index';
 
 describe('Shared Button Component', () => {
-    describe('Checking PropTypes',()=>{
-        it('Should NOT throw a warning',()=>{
-            const expectedProps = {
-                buttonText: 'Example Button Text',
-                emitEven: () => {}
-            }
+  describe('Checking PropTypes', () => {
+    it('Should NOT throw a warning', () => {
+      const expectedProps = {
+        buttonText: 'Example Button Text',
+        emitEven: () => {},
+      };
 
-            const propsErr = checkProps(SharedButton, expectedProps);
-            expect(propsErr).toBeUndefined();
-        })
-    })
+      const propsErr = checkProps(SharedButton, expectedProps);
+      expect(propsErr).toBeUndefined();
+    });
+  });
 
-    describe('Renders',()=>{
-        let wrapper;
-        beforeEach(()=>{
-            const props = {
-              buttonText: 'Example Button Text',
-              emitEven: () => {},
-            };
-            wrapper = shallow(<SharedButton {...props}/>);
+  describe('Renders', () => {
+    let wrapper;
+    let mockFunc;
+    beforeEach(() => {
+      mockFunc = jest.fn();
+      const props = {
+        buttonText: 'Example Button Text',
+        emitEvent: mockFunc,
+      };
+      wrapper = shallow(<SharedButton {...props} />);
+    });
 
-        });
+    it('Should render a button', () => {
+      const button = findByTestAttr(wrapper, 'buttonComponent');
+      expect(button.length).toBe(1);
+    });
 
-        it('Should render a button',()=>{
-            const button = findByTestAttr(wrapper,'buttonComponent');
-            expect(button.length).toBe(1)
-        })
-    })
-})
+    it('Should emit callback on click event', () => {
+      const button = findByTestAttr(wrapper, 'buttonComponent');
+      // simulate handle the Events
+      button.simulate('click');
+      const callback = mockFunc.mock.calls.length;
+      expect(callback).toBe(1);
+    });
+  });
+});
